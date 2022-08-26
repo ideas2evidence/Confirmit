e0b8e606bfc64e323ed29865ba8a0276aa9c774d
Øivind Skjervheim <oivind.skjervheim@ideas2evidence.com>
2022-08-26 15:57:39 +0200

    minor adjustments and documentation

--- count keyboard strokes/code.js
+++ count keyboard strokes/code.js
@@ -2,7 +2,7 @@
 // Confirmit-nodes
 //  - Page
 //  -- q1 (open text for the respondent to write his answer)
-//  -- q1_count (numeric node for storing number of key strokes)
+//  -- q1_count (numeric node for storing number of key strokes. Normal question type, but hide with CSS)
 
 
 document.getElementById('q1_count_input').disabled = "true";
@@ -18,9 +18,11 @@ function countKeyStrokes(html, event) {
     node.value = number; // write new value back to input field
 }
 
-let htmlElem = document.querySelector("html");
 
-htmlElem.addEventListener("keydown", function (e) {
-    countKeyStrokes(htmlElem, e)
+
+let nodeElement = document.querySelector("#q1_input");
+
+nodeElement.addEventListener("keydown", function (e) {
+    countKeyStrokes(nodeElement, e)
 
 })
