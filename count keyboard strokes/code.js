15f705c156e961033812fc6da8cf5512b1c87483
Øivind Skjervheim <oivind.skjervheim@ideas2evidence.com>
2022-08-26 15:46:13 +0200

    fixes to mouse clicks and keystroke count

--- count keyboard strokes/code.js
+++ count keyboard strokes/code.js
@@ -1,25 +1,26 @@
 
 // Confirmit-nodes
 //  - Page
-//  -- q1 (open text)
-//  -- q1_count (numeric)
+//  -- q1 (open text for the respondent to write his answer)
+//  -- q1_count (numeric node for storing number of key strokes)
 
 
-document.getElementById('q1_count').disabled = "true"; 
+document.getElementById('q1_count_input').disabled = "true";
 
 function countKeyStrokes(html, event) {
-    var node = document.getElementById("q1_count");
-    var old = node.value;
-    if(old === undefined){
-        old = 0
-    }
-    console.log("value: " + old)
-    node.value = old + 1
+    var node = document.getElementById("q1_count_input"); // Get input field of node
+    var number = node.value; // Get current value from input field
+    if (number === undefined) { //if no value, the input is undefined.
+        number = 0;
+    } 
+    number++; // increment by 1
+    //console.log("value: " + number);
+    node.value = number; // write new value back to input field
 }
 
-let htmlElem = document.querySelector("html"); 
+let htmlElem = document.querySelector("html");
 
-htmlElem.addEventListener("keydown", function(e) {
+htmlElem.addEventListener("keydown", function (e) {
     countKeyStrokes(htmlElem, e)
 
 })
---
+++ mouse click coordinates/code.js
@@ -0,0 +1,25 @@
+
+// Confirmit-nodes
+//  - q1 (open text)
+
+// Note: Coordinates are dependent on screen resolution
+
+document.getElementById('q1_input').disabled= "true"; 
+
+function getMousePosition(html, event) { 
+    let rect = html.getBoundingClientRect(); 
+    let x = event.clientX - rect.left; 
+    let y = event.clientY - rect.top; 
+    console.log("Coordinate x: " + x, 
+        "Coordinate y: " + y); 
+    var outputCoordinates = "X:"+ x +", Y:"+ y +"\n"; 
+    var textArea = document.getElementById("q1_input"); 
+    var old = textArea.value; 
+    textArea.value = old + outputCoordinates; 
+} 
+
+let htmlElem = document.querySelector("html"); 
+
+htmlElem.addEventListener("mousedown", function (e) { 
+    getMousePosition(htmlElem, e); 
+});
