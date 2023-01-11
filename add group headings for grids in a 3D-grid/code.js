1cb62b4dba3d699f686a109991a22e8f60712d5b
jbwettergreen <joachim.wettergreen@gmail.com>
2023-01-11 21:12:42 +0100

    added file 'add group headings for grids in a 3D-grid

---
+++ add group headings for grids in a 3D-grid/code.js
@@ -0,0 +1,17 @@
+// Requirements:
+// 3D grid
+// at least one grid within 3D-grid
+
+// Why it's necessary:
+// Group headings for grid nodes within 3D-grid nodes won't show by default in Confirmit,
+// and must be added manually
+
+// For this example specifically:
+// q44 (3D-grid)
+// q44a (grid within q44)
+// q44b (grid within q44)
+
+//Add to 3D-grid node specific JS-script
+document.getElementById("desktop_q44a_text").innerHTML = "YOUR TEXT HERE"
+document.getElementById("desktop_q44b_text").innerHTML = "YOUR TEXT HERE"
+
